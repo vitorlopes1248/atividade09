7a0549fef669f8c4a068cfc5ec87718d3dbd09c1
vitorlopes1248 <vitorlopes1248@gmail.com>
2024-09-04 16:07:22 -0300

    atividade09 media de litros por kilometro

---
+++ atividade09.py
@@ -0,0 +1,9 @@
+# Um motorista deseja calcular o consumo médio de combustível do seu carro. Crie um programa que receba a quantidade de quilômetros percorridos e a quantidade de litros de combustível consumidos, e calcule o consumo médio (km por litro).
+
+km = int(input("Quantos kilometros foram pecorridos: "))
+litros = int(input("Quantos litros foram colocados no carro: "))
+
+media = km/litros
+
+print("Media de litros pr kilometros: ", media)
+
